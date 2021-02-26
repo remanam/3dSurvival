00581406aed03ecbda40050c6604ffc790b0ece5
unknown <remanam@mail.ru>
2021-02-26 10:21:01 +0300

    3:10:44

---
+++ Assets/Scripts/WeaponScripts/WeaponHandler.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim
+{
+    NONE,
+    SELF_AIM, // bow, spear.  They aim every time
+    AIM, // Aim when press a button
+}
+
+public enum WeaponFireType
+{
+    SINGLE, // pistols, one fire every click
+    MULTIPLE // fast weapons, fire while holding a click
+}
+
+public enum WeaponBulletType
+{
+    BULLET,
+    ARROW,
+    SPEAR,
+    NONE
+}
+
+public class WeaponHandler : MonoBehaviour
+{
+    private Animator anim;
+
+    public WeaponAim weaponAim;
+
+    [SerializeField]
+    private GameObject muzzleFlash;
+
+    [SerializeField]
+    private AudioSource shootSound, reloadSound;
+
+    public WeaponFireType fireType;
+
+    public WeaponBulletType bulletType;
+
+    public GameObject attack_Point; // For axe, to determine whether we hit collider or not
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void ShootAnimation()
+    {
+        anim.SetTrigger(AnimationTags.SHOOT_TRIGGER);
+    }
+
+    public void Aim(bool canAim)
+    {
+        anim.SetBool(AnimationTags.AIM_PARAMETER, canAim);
+    }
+
+    void Turn_On_MuzzleFlash()
+    {
+        muzzleFlash.SetActive(true);
+    }
+
+    void Turn_Off_MuzzleFlash()
+    {
+        muzzleFlash.SetActive(false);
+    }
+
+    void Play_ShootSound()
+    {
+        shootSound.Play();
+    }
+
+    void Play_ReloadSound()
+    {
+        reloadSound.Play();
+    }
+
+    void Turn_On_AttackPoint()
+    {
+        attack_Point.SetActive(true);
+    }
+
+    void Turn_Off_AttackPoint()
+    {
+        if (attack_Point.activeInHierarchy) {
+            attack_Point.SetActive(false);
+        }
+    }
+}
